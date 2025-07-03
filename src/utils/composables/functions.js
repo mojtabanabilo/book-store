746fe2f5bee3e05bcd0d760ca4f09ed45a7e1381
mojtaba nabilu <mojjtaba00@gmail.com>
2025-07-03 16:51:42 +0330

    some change

--- src/utils/composables/functions.js
+++ src/utils/composables/functions.js
@@ -2,7 +2,7 @@ export default function hooks() {
   const filterTitle = (str) => {
     const words = str.split("");
     if (words.length > 15) {
-      return words.slice(0, 17).join("") + "...";
+      return "..." + words.slice(0, 17).join("");
     } else {
       return str;
     }
