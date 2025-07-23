369ca5f45c811f201d363fabd9d36cb40ea907e9
mojtaba nabilu <mojjtaba00@gmail.com>
2025-07-23 18:42:27 +0330

    added sticky position to thead table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,25 +1,27 @@
 <template>
-    <table class="table">
-        <thead>
-            <tr>
-                <th v-for="(col, index) in columns" :key="index">
-                    {{ col }}
-                </th>
-                <th>...</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(row, rowIndex) in data" :key="rowIndex">
-                <td v-for="(col, colIndex) in columns" :key="colIndex">
-                    {{ row[col] }}
-                </td>
-                <td class="actions">
-                    <img :src="edit" alt="edit" @click="emit('show-modal', 'edit')"  />
-                    <img :src="trash" alt="trash" @click="emit('show-modal', 'delete')" />
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="table-wrapper">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th v-for="(col, index) in columns" :key="index">
+                        {{ col }}
+                    </th>
+                    <th>...</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, rowIndex) in data" :key="rowIndex">
+                    <td v-for="(col, colIndex) in columns" :key="colIndex">
+                        {{ row[col] }}
+                    </td>
+                    <td class="actions">
+                        <img :src="edit" alt="edit" @click="emit('show-modal', 'edit')" />
+                        <img :src="trash" alt="trash" @click="emit('show-modal', 'delete')" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script setup>
