098e9cf11f1d5c0260ec37a9331aa754238f853d
mojtaba nabilu <mojjtaba00@gmail.com>
2025-07-23 18:03:11 +0330

    some change BookList.vue

--- src/pages/BookList.vue
+++ src/pages/BookList.vue
@@ -1,40 +1,55 @@
 <template>
-    <div class="book-container" :dir="directionLang" v-if="currentBook !== null">
-        <div class="book-wrapper">
-            <div class="image-section">
-                <img :src="picture" alt="book-image" class="book-picture" />
-            </div>
+    <section class="book-container" :dir="directionLang" v-if="currentBook">
+        <article class="book-wrapper">
+            <!-- تصویر کتاب -->
+            <figure class="image-section">
+                <img :src="picture" alt="Book Image" class="book-picture" />
+            </figure>
 
-            <div class="info-section">
-                <h2>{{ currentBook.title }}</h2>
-                <p class="author">{{ currentBook.author }}</p>
+            <!-- اطلاعات کتاب -->
+            <section class="info-section">
+                <header>
+                    <h2>{{ currentBook.title }}</h2>
+                    <p class="author">{{ currentBook.author }}</p>
+                </header>
+
                 <p class="summary">{{ currentBook.summary }}</p>
+
                 <Button :label="t('roots.bookList.btnPurchase')" :styleProps="{ width: '200px', height: '50px' }"
                     :icon="cartIcon" />
-                <div class="book-details">
-                    <div><span>{{ t('roots.bookList.price') }}:</span>
+
+                <!-- مشخصات -->
+                <section class="book-details">
+                    <div class="detail-item">
+                        <span>{{ t('roots.bookList.price') }}:</span>
                         <p>{{ currentBook.price }}</p>
                     </div>
-                    <div><span>{{ t('roots.bookList.ISBN') }}:</span>
+                    <div class="detail-item">
+                        <span>{{ t('roots.bookList.ISBN') }}:</span>
                         <p>978-62281720</p>
                     </div>
-                    <div><span>{{ t('roots.bookList.format') }}:</span>
+                    <div class="detail-item">
+                        <span>{{ t('roots.bookList.format') }}:</span>
                         <p>رقعی</p>
                     </div>
-                    <div><span>{{ t('roots.bookList.numberPages') }}:</span>
+                    <div class="detail-item">
+                        <span>{{ t('roots.bookList.numberPages') }}:</span>
                         <p>281</p>
                     </div>
-                    <div><span>{{ t('roots.bookList.yearPublication') }}:</span>
+                    <div class="detail-item">
+                        <span>{{ t('roots.bookList.yearPublication') }}:</span>
                         <p>1389</p>
                     </div>
-                    <div><span>{{ t('roots.bookList.printSeries') }}:</span>
+                    <div class="detail-item">
+                        <span>{{ t('roots.bookList.printSeries') }}:</span>
                         <p>17</p>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                </section>
+            </section>
+        </article>
+    </section>
 </template>
+
 
 <script setup>
 import { useRoute } from 'vue-router';
